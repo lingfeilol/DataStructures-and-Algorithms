8743b13a3862a455dc058724da981759ae150c9c
lingfeilol <lingfeilol@163.com>
2020-02-10 23:14:41 +0800

    重新测试

--- Heap/test.c
+++ Heap/test.c
@@ -1,18 +1,17 @@
 #include "Heap.h"
-
-
 int main()
 {
 	Heap hp;
-	int arr[] = { 1, 8, 5, 3, 4, 9, 7, 2, 0, 6 };
+	int arr[] = {1,2,3,4,5,6,7,8,9,0};
 	int len = sizeof(arr) / sizeof(int);
 	HeapCreate(&hp, arr, len);
 	HeapPrintf(&hp);
 
-	//HeapPush(&hp, 16);//插入一个元素
-	//HeapPrintf(&hp);
+	HeapPush(&hp, 16);//插入一个元素
+	HeapPrintf(&hp);
 	printf("堆顶元素：%d \n", HeapTop(&hp));
 	printf("堆里的元素个数：%d\n", HeapSize(&hp));
+
 	HeapSort(arr,len);//对数组进行堆排序 升序
 	for (int i = 0; i < len; i++)
 	{
